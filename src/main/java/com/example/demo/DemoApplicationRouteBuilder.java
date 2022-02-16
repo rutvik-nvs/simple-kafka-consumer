bb693156dff2eebca03edc2a8a9a65273b7ff00c
rutvik-nvs <rutvik.panchal@nvsconsulting.io>
2022-02-16 03:03:15 -0600

    Added log caching for solving multiline logs

--- src/main/java/com/example/demo/DemoApplicationRouteBuilder.java
+++ src/main/java/com/example/demo/DemoApplicationRouteBuilder.java
@@ -1,19 +1,58 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoApplicationRouteBuilder extends RouteBuilder {
 
+    HashMap<String, Object> hashMap         = new HashMap<String, Object>();
+
+    private static String   regex           = "\\d{4}-\\d{1,2}-\\d{1,2}";
+    private static Pattern  regexPattern    = Pattern.compile(regex);
+
     @Override
     public void configure() throws Exception {
 
         // Kafka Route for consuming messages and logging them to the console
         from("kafka:{{kafka.topic}}?brokers={{kafka.host}}:{{kafka.port}}&reconnectBackoffMaxMs={{kafka.timeout}}&groupId={{kafka.groupId}}")
+            
             .setBody().jsonpath("$.message", true)
-            .log(LoggingLevel.INFO, "Kafka", "${body}")
+            .setHeader("namespace").jsonpath("$.kubernetes.namespace_name", true)
+
+            .process(new Processor() {
+                @Override
+                public void process(Exchange exchange) throws Exception
+                {
+                    String body =       exchange.getIn().getBody().toString();
+                    String namespace =  exchange.getIn().getHeader("namespace").toString();
+
+                    // Regex lookup
+                    if(regexPattern.matcher(body.substring(0, 24)).matches()){
+                        exchange.getIn().setBody(hashMap.get("namespace") != null ? hashMap.get("namespace").toString() : "");
+                        if(hashMap.get("namespace") != null){
+                            exchange.getIn().setHeader("shouldCallErrorDb", true);
+                        }
+                        else{
+                            exchange.getIn().setHeader("shouldCallErrorDb", false);
+                        }
+                    }
+                    else{
+                        hashMap.put(namespace, hashMap.get("namespace") != null ? (hashMap.get("namespace").toString() + body) : body);
+                    }
+                }
+            })
+
+            .choice()
+                .when().simple("${headers.shouldCallErrorDb} == true")
+                    .log(LoggingLevel.INFO, "Kafka", "${body}")
+            .end()
             .to("mock:done");
     }
 }
